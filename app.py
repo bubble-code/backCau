d806f25e9c45f83d73ff1e746bddfe271ad88090
Alejandro Obregón Rodríguez <a.obregon@favram.local>
2023-09-16 06:53:20 -0700

    casi terminado falta conectar la api de chatgpt

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from model.Query import Query 
+from model.Query import Query
 
 
 app = Flask(__name__)
@@ -11,34 +11,42 @@ query_instance = Query()
 # Carga los modelos de procesamiento de lenguaje antes de la primera solicitud
 # @app.before_first_request
 
-@app.route('/',methods=['POST'])
+@app.route('/', methods=['POST'])
 def hello():
-    data = request.json
-    query = data['query']   
-    print(query)
     returnList = []
+    data = request.json
+    query = data['query']
+    if query is None or query.strip() == "":
+        returnList.append({
+            "section_name": "Sin preguntas...",
+            "sentence_text": "Debe escribir una pregunta o texto",
+            "similarity": 0
+        })
+        return jsonify(returnList)
+    print(query)
     similarity_list = query_instance.query(query)
     for seccion_name, sentence_text, similarity in similarity_list[:5]:
-            returnList.append({
+        returnList.append({
             "section_name": seccion_name,
             "sentence_text": sentence_text,
             "similarity": similarity
-                            })
+        })
     return jsonify(returnList)
 
 
-@app.route('/') 
-def GetHome():
-    print('Cargando')    
-    returnList = []
-    similarity_list = query_instance.query("¿Como copiar articulos?")
-    for seccion_name, sentence_text, similarity in similarity_list[:5]:
-            returnList.append({
-            "section_name": seccion_name,
-            "sentence_text": sentence_text,
-            "similarity": similarity
-                            })
-    return jsonify(returnList)
+# @app.route('/')
+# def GetHome():
+#     print('Cargando')
+#     returnList = []
+#     similarity_list = query_instance.query("¿Como copiar articulos?")
+#     for seccion_name, sentence_text, similarity in similarity_list[:5]:
+#         returnList.append({
+#             "section_name": seccion_name,
+#             "sentence_text": sentence_text,
+#             "similarity": similarity
+#         })
+#     return jsonify(returnList)
+
 
 # Ejecuta la aplicación Flask
 if __name__ == '__main__':
